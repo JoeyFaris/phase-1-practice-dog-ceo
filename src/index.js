827e28c3f906d05c6160f2a4b41096e01699815f
Joey Faris <joeyfaris1@yahoo.com>
2022-06-14 11:47:43 -0700

    commit tree d02a2d5e2f36a878c279943f8698d49820b7e546

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('%c HI', 'color: firebrick')
+
+function loadImages() {
+fetch('https://dog.ceo/api/breeds/image/random/4')
+.then(res => res.json)
+.then(data => {
+    data.forEach(image => createImage(image))
+
+})
+}
